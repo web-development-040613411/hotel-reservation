acf52a3fd6c6d820564ca20cb14ca63542a31970
FuseSN2003 <sukavit.fuse2003@gmail.com>
2024-09-16 16:27:12 +0700

    Change backend port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().listen(3000);
+const app = new Elysia().listen(3001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`

2fff6bd748b00f1dd1d85a7acb9d5daf4575ad5b
Mok <mokmaard646@gmail.com>
2024-09-17 17:23:01 +0700

    add route to index .ts

--- server/src/routes/admin/index.ts
+++ server/src/routes/admin/index.ts
@@ -1,4 +1,7 @@
 import Elysia from "elysia";
 import { roomTypeRoutes } from "./roomType";
+import { roomRoutes } from "./room";
 
-export const adminRoutes = new Elysia({ prefix: "/admin" }).use(roomTypeRoutes);
+export const adminRoutes = new Elysia({ prefix: "/admin" })
+  .use(roomTypeRoutes)
+  .use(roomRoutes);

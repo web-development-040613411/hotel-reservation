2588fd83fc7b3e44a2669deb035977b31bba472f
Ohmmy569 <Ohmmy569@gmail.com>
2024-09-18 10:44:44 +0700

    change ide to 10 sec

--- server/src/libs/db.ts
+++ server/src/libs/db.ts
@@ -3,6 +3,6 @@ import postgres from 'postgres';
 export const sql = postgres(process.env.DATABASE_URL!, {
   ssl: 'allow',
   max: 10,
-  idle_timeout: 5,
+  idle_timeout: 30,
   connect_timeout: 5,
 });

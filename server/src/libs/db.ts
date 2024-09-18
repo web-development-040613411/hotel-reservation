0e622428e73e251a1a53b2122655943c2e291249
Ohmmy569 <Ohmmy569@gmail.com>
2024-09-18 10:50:18 +0700

    just idle and lifetime

--- server/src/libs/db.ts
+++ server/src/libs/db.ts
@@ -2,7 +2,6 @@ import postgres from 'postgres';
 
 export const sql = postgres(process.env.DATABASE_URL!, {
   ssl: 'allow',
-  max: 10,
   idle_timeout: 30,
-  connect_timeout: 5,
+  max_lifetime: 30 * 60,
 });

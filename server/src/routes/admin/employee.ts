6eeedc637d54459722bb0ef9329796a1f4861d27
Ohmmy569 <Ohmmy569@gmail.com>
2024-09-18 13:21:11 +0700

    remove try catch

--- server/src/routes/admin/employee.ts
+++ server/src/routes/admin/employee.ts
@@ -7,13 +7,9 @@ import {
 } from '@/libs/validation';
 import { join } from 'path';
 import { unlink } from 'node:fs/promises';
-import { ZodError } from 'zod';
 import { uploadFile } from '@/libs/uploadFile';
 
 export const employeeRoutes = new Elysia({ prefix: '/employees' })
-    .onError(({ code, error }) => {
-        return new Response(error.toString());
-    })
     .get('/', async ({ set }) => {
         try {
             const employees = await sql`SELECT * FROM employee`;
@@ -30,247 +26,21 @@ export const employeeRoutes = new Elysia({ prefix: '/employees' })
         }
     })
     .get('/:id', async ({ params: { id }, set }) => {
-        try {
-            const employee = await sql`SELECT * FROM employee WHERE id=${id}`;
-            if (employee.length === 0) {
-                set.status = 404;
-                return {
-                    status: 'error',
-                    message: 'Employee not found',
-                };
-            }
-            return {
-                status: 'success',
-                data: employee[0],
-            };
-        } catch (error) {
-            set.status = 500;
+        const employee = await sql`SELECT * FROM employee WHERE id=${id}`;
+        if (employee.length === 0) {
+            set.status = 404;
             return {
                 status: 'error',
-                message: 'Internal server error, please try again later',
+                message: 'Employee not found',
             };
         }
+        return {
+            status: 'success',
+            data: employee[0],
+        };
     })
     .post('/', async ({ body, set }) => {
-        try {
-            const validateData = addEmployeeSchema.safeParse(body);
-            if (!validateData.success) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: validateData.error.errors[0].message,
-                };
-            }
-            const {
-                user_name,
-                firstname,
-                lastname,
-                date_of_birth,
-                password,
-                confirm_password,
-                role,
-                image,
-            } = validateData.data;
-
-            const IsEmployeeExist =
-                await sql`SELECT * FROM employee WHERE user_name=${user_name}`;
-            if (IsEmployeeExist.length > 0) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: 'Employee already exists',
-                };
-            }
-            if (!image) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: 'Image is required',
-                };
-            }
-            if (password !== confirm_password) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: 'Password and confirm password does not match',
-                };
-            }
-            const Hash_password = await Bun.password.hash(password, {
-                algorithm: 'bcrypt',
-                cost: 10,
-            });
-
-            const url = await uploadFile(image);
-            if (!url) {
-                set.status = 500;
-                return {
-                    status: 'error',
-                    message: 'Internal server error, please try again later',
-                };
-            }
-
-            const Query_roles = await sql`SELECT enum_range(NULL::role);`;
-            const all_roles = Object.values(Query_roles[0].enum_range);
-            if (!all_roles.includes(role)) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: 'Role does not exist',
-                };
-            }
-
-            const employee =
-                await sql`INSERT INTO employee (user_name, firstname, lastname, date_of_birth, password, role , profile_picture) VALUES (${user_name}, ${firstname}, ${lastname}, ${date_of_birth}, ${Hash_password}, ${role} , ${url})`;
-            return {
-                status: 'success',
-                message: 'Employee added successfully',
-            };
-        } catch (error) {
-            if (error instanceof ZodError) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: error.errors[0].message,
-                };
-            }
-
-            set.status = 500;
-            return {
-                status: 'error',
-                message: 'Internal server error, please try again later',
-            };
-        }
-    })
-
-    .put('/:id', async ({ params: { id }, body, set }) => {
-        try {
-            const validateData = updateEmployeeSchema.safeParse(body);
-            if (!validateData.success) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: validateData.error.errors[0].message,
-                };
-            }
-            const {
-                user_name,
-                firstname,
-                lastname,
-                date_of_birth,
-                role,
-                image,
-            } = validateData.data;
-
-            const [existsingEmployee] =
-                await sql`SELECT * FROM employee WHERE id=${id}`;
-            if (!existsingEmployee) {
-                set.status = 404;
-                return {
-                    status: 'error',
-                    message: 'Employee not found',
-                };
-            }
-
-            let url;
-            if (image) {
-                const path = join(
-                    '.',
-                    process.env.UPLOAD_FOLDER!,
-                    existsingEmployee.profile_picture.split('/').pop()
-                );
-                await unlink(path);
-
-                url = await uploadFile(image);
-                if (!url) {
-                    set.status = 500;
-                    return {
-                        status: 'error',
-                        message:
-                            'Internal server error, please try again later',
-                    };
-                }
-            } else {
-                url = existsingEmployee.profile_picture;
-            }
-            if (!url) {
-                set.status = 500;
-                return {
-                    status: 'error',
-                    message: 'Internal server error, please try again later',
-                };
-            }
-
-            const Query_roles = await sql`SELECT enum_range(NULL::role);`;
-            const all_roles = Object.values(Query_roles[0].enum_range);
-            if (!all_roles.includes(role)) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: 'Role does not exist',
-                };
-            }
-
-            const employee =
-                await sql`UPDATE employee SET user_name=${user_name}, firstname=${firstname}, lastname=${lastname}, date_of_birth=${date_of_birth}, role=${role} , profile_picture=${url} WHERE id=${id}`;
-            return {
-                status: 'success',
-                message: 'Employee updated successfully',
-            };
-        } catch (error) {
-            if (error instanceof ZodError) {
-                set.status = 400;
-                return {
-                    status: 'error',
-                    message: error.errors[0].message,
-                };
-            }
-
-            set.status = 500;
-            return {
-                status: 'error',
-                message: 'Internal server error, please try again later',
-            };
-        }
-    })
-
-    .delete('/:id', async ({ params: { id }, set }) => {
-        try {
-            const [employee] = await sql`SELECT * FROM employee WHERE id=${id}`;
-            if (!employee) {
-                set.status = 404;
-                return {
-                    status: 'error',
-                    message: 'Employee not found',
-                };
-            }
-            const path = join(
-                '.',
-                process.env.UPLOAD_FOLDER!,
-                employee.profile_picture.split('/').pop()
-            );
-
-            await sql`DELETE FROM employee WHERE id=${id}`;
-            await unlink(path);
-            set.status = 200;
-            return {
-                status: 'success',
-                message: 'Employee deleted successfully',
-            };
-        } catch (error) {
-            console.log(error);
-            set.status = 500;
-            return {
-                status: 'error',
-                message: 'Internal server error, please try again later',
-            };
-        }
-    });
-
-export const resetPasswordRoutes = new Elysia({
-    prefix: '/resetEmployeePassword',
-}).put('/:id', async ({ params: { id }, body, set }) => {
-    try {
-        const validateData = ResetPasswordSchema.safeParse(body);
+        const validateData = addEmployeeSchema.safeParse(body);
         if (!validateData.success) {
             set.status = 400;
             return {
@@ -278,14 +48,31 @@ export const resetPasswordRoutes = new Elysia({
                 message: validateData.error.errors[0].message,
             };
         }
-        const { password, confirm_password } = validateData.data;
+        const {
+            user_name,
+            firstname,
+            lastname,
+            date_of_birth,
+            password,
+            confirm_password,
+            role,
+            image,
+        } = validateData.data;
 
-        const employee = await sql`SELECT * FROM employee WHERE id=${id}`;
-        if (employee.length === 0) {
-            set.status = 404;
+        const IsEmployeeExist =
+            await sql`SELECT * FROM employee WHERE user_name=${user_name}`;
+        if (IsEmployeeExist.length > 0) {
+            set.status = 400;
             return {
                 status: 'error',
-                message: 'Employee not found',
+                message: 'Employee already exists',
+            };
+        }
+        if (!image) {
+            set.status = 400;
+            return {
+                status: 'error',
+                message: 'Image is required',
             };
         }
         if (password !== confirm_password) {
@@ -299,16 +86,161 @@ export const resetPasswordRoutes = new Elysia({
             algorithm: 'bcrypt',
             cost: 10,
         });
-        await sql`UPDATE employee SET password=${Hash_password} WHERE id=${id}`;
+
+        const url = await uploadFile(image);
+        if (!url) {
+            set.status = 500;
+            return {
+                status: 'error',
+                message: 'Internal server error, please try again later',
+            };
+        }
+
+        const Query_roles = await sql`SELECT enum_range(NULL::role);`;
+        const all_roles = Object.values(Query_roles[0].enum_range);
+        if (!all_roles.includes(role)) {
+            set.status = 400;
+            return {
+                status: 'error',
+                message: 'Role does not exist',
+            };
+        }
+
+        const employee =
+            await sql`INSERT INTO employee (user_name, firstname, lastname, date_of_birth, password, role , profile_picture) VALUES (${user_name}, ${firstname}, ${lastname}, ${date_of_birth}, ${Hash_password}, ${role} , ${url})`;
         return {
             status: 'success',
-            message: 'Password updated successfully',
+            message: 'Employee added successfully',
         };
-    } catch (error) {
-        set.status = 500;
+    })
+
+    .put('/:id', async ({ params: { id }, body, set }) => {
+        const validateData = updateEmployeeSchema.safeParse(body);
+        if (!validateData.success) {
+            set.status = 400;
+            return {
+                status: 'error',
+                message: validateData.error.errors[0].message,
+            };
+        }
+        const { user_name, firstname, lastname, date_of_birth, role, image } =
+            validateData.data;
+
+        const [existsingEmployee] =
+            await sql`SELECT * FROM employee WHERE id=${id}`;
+        if (!existsingEmployee) {
+            set.status = 404;
+            return {
+                status: 'error',
+                message: 'Employee not found',
+            };
+        }
+
+        let url;
+        if (image) {
+            const path = join(
+                '.',
+                process.env.UPLOAD_FOLDER!,
+                existsingEmployee.profile_picture.split('/').pop()
+            );
+            await unlink(path);
+
+            url = await uploadFile(image);
+            if (!url) {
+                set.status = 500;
+                return {
+                    status: 'error',
+                    message: 'Internal server error, please try again later',
+                };
+            }
+        } else {
+            url = existsingEmployee.profile_picture;
+        }
+        if (!url) {
+            set.status = 500;
+            return {
+                status: 'error',
+                message: 'Internal server error, please try again later',
+            };
+        }
+
+        const Query_roles = await sql`SELECT enum_range(NULL::role);`;
+        const all_roles = Object.values(Query_roles[0].enum_range);
+        if (!all_roles.includes(role)) {
+            set.status = 400;
+            return {
+                status: 'error',
+                message: 'Role does not exist',
+            };
+        }
+
+        const employee =
+            await sql`UPDATE employee SET user_name=${user_name}, firstname=${firstname}, lastname=${lastname}, date_of_birth=${date_of_birth}, role=${role} , profile_picture=${url} WHERE id=${id}`;
+        return {
+            status: 'success',
+            message: 'Employee updated successfully',
+        };
+    })
+
+    .delete('/:id', async ({ params: { id }, set }) => {
+        const [employee] = await sql`SELECT * FROM employee WHERE id=${id}`;
+        if (!employee) {
+            set.status = 404;
+            return {
+                status: 'error',
+                message: 'Employee not found',
+            };
+        }
+        const path = join(
+            '.',
+            process.env.UPLOAD_FOLDER!,
+            employee.profile_picture.split('/').pop()
+        );
+
+        await sql`DELETE FROM employee WHERE id=${id}`;
+        await unlink(path);
+        set.status = 200;
+        return {
+            status: 'success',
+            message: 'Employee deleted successfully',
+        };
+    });
+
+export const resetPasswordRoutes = new Elysia({
+    prefix: '/resetEmployeePassword',
+}).put('/:id', async ({ params: { id }, body, set }) => {
+    const validateData = ResetPasswordSchema.safeParse(body);
+    if (!validateData.success) {
+        set.status = 400;
         return {
             status: 'error',
-            message: 'Internal server error, please try again later',
+            message: validateData.error.errors[0].message,
         };
     }
+    const { password, confirm_password } = validateData.data;
+
+    const employee = await sql`SELECT * FROM employee WHERE id=${id}`;
+    if (employee.length === 0) {
+        set.status = 404;
+        return {
+            status: 'error',
+            message: 'Employee not found',
+        };
+    }
+    if (password !== confirm_password) {
+        set.status = 400;
+        return {
+            status: 'error',
+            message: 'Password and confirm password does not match',
+        };
+    }
+    const Hash_password = await Bun.password.hash(password, {
+        algorithm: 'bcrypt',
+        cost: 10,
+    });
+    await sql`UPDATE employee SET password=${Hash_password} WHERE id=${id}`;
+    return {
+        status: 'success',
+        message: 'Password updated successfully',
+    };
 });

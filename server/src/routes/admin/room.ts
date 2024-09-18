674a7b19f6b64e5a2c4a636d94736431816671a1
Mok <mokmaard646@gmail.com>
2024-09-18 21:34:13 +0700

    refactor : add on error.

--- server/src/routes/admin/room.ts
+++ server/src/routes/admin/room.ts
@@ -1,34 +1,43 @@
 import { sql } from "@/libs/db";
+import { createRoomSchema } from "@/libs/validation";
 import Elysia, { t } from "elysia";
 import postgres from "postgres";
 
 export const roomRoutes = new Elysia({ prefix: "/room" })
+  .onError(({error}) => {
+    if (error instanceof postgres.PostgresError && error.code == "23505") {
+      if (error.constraint_name == "room_number_key") {
+        return {
+          status: "error",
+          message: "This room number have already used.",
+        };
+      }
+    }
+  })
   .post(
     "/create",
     async ({ body }) => {
       const { number, type_id } = body;
 
-      try {
-        await sql`      INSERT INTO rooms(number, type_id) 
-                        VALUES (${number}, ${type_id})`;
-      } catch (error) {
-        if (error instanceof postgres.PostgresError && error.code == "23505") {
-          if (error.constraint_name == "room_number_key") {
-            return {
-              status: "error",
-              message: "This room number have already used.",
-            };
-          }
-        }
+      const validation = createRoomSchema.safeParse({
+        number: number,
+        type_id: type_id
+      })
 
+      if ( !validation.success ) {
+        const error = validation.error.issues[0].message;
         return {
-          status : "error"
+          status : "error",
+          message : error
         }
       }
 
+      await sql`INSERT INTO rooms(number, type_id) 
+                VALUES (${number}, ${type_id})`;
+
       return {
         status: "success",
-        message: "You have already created new room.",
+        message: "You have already a created new room.",
       };
     },
     {
@@ -38,81 +47,146 @@ export const roomRoutes = new Elysia({ prefix: "/room" })
       }),
     }
   )
-  .get("/", async () => {
-    const res = await sql`SELECT * FROM room;`;
-    return {
-      status: "success",
-      data: { res },
-    };
-  })
-  .put("/update", async ( {query} ) => {
-    const { id, number, type_id, status } = query;
-
-    try {
-      await sql`
-        UPDATE rooms
-        SET number = ${number},
-        type_id = ${type_id},
-        current_status = ${status}
-        WHERE id = ${id}
-      `
-    } catch (error) {
-      if ( error instanceof postgres.PostgresError ) {
-        if ( error.code == "22P02") {
-          return {
-            status : "error",
-            message : "type wrong"
-          }
-        } 
-
-        if ( error.code == "23505" && error.constraint_name == "room_number_key" ) {
-          return {
-            status : "error",
-            message : "This room number is already taken."
+  .get(
+    "/",
+    async ({set}) => {
+      try {
+        const res = await sql`SELECT room.number, room.current_status, room_type.name,  room_type.price
+                      FROM rooms
+                      INNER JOIN room_type
+                      ON rooms.type_id = room_type.id;`;
+        return {
+          status: "success",
+          data: res,
+        };
+      } catch (error) {
+        set.status = 400;
+        if (error instanceof postgres.PostgresError) {
+          if (error.code == "22P02") {
+            return {
+              status: "error",
+              message: "id is not uuid.",
+            };
           }
         }
       }
+    }
+  )
+  .get("/:id" , async ({ params,set }) => {
+    const { id } = params;
 
-      console.log( error );
-
+    try {
+      const res = await sql`SELECT rooms.number, rooms.current_status, room_type.name, room_type.price 
+                            FROM rooms
+                            INNER JOIN room_type
+                            ON rooms.type_id = room_type.id
+                            WHERE rooms.id = ${id}`
       return {
-        status : "error",
-        message : error
+        status: "success",
+        data: res,
+      };
+    } catch (error) {
+      set.status = 400;
+      if (error instanceof postgres.PostgresError) {
+        if (error.code == "22P02") {
+          return {
+            status: "error",
+            message: "id is not uuid.",
+          };
+        }
       }
     }
-
-    return {
-      status: "success",
-      message: "Update successfully.",
-    };
   }, {
-    query : t.Object({
-      id : t.String(),
-      number : t.String(),
-      type_id : t.String(),
-      status : t.String()
-    })
+    params : t.Object( {
+      id : t.String()
+      }
+    )
   })
+  .patch(
+    "/update",
+    async ({ body }) => {
+      const { id, number, type_id, status } = body;
+
+      if (status == "maintenance" || status == "off_market") {
+        const roomIDs = await sql`
+          SELECT * 
+          FROM reservation
+          WHERE id = ${id}
+        `;
+
+        return { roomIDs };
+      }
+
+      // try {
+      //   await sql.begin(async (sql) => {});
+
+      //   await sql`
+      //   UPDATE rooms
+      //   SET number = ${number},
+      //   type_id = ${type_id},
+      //   current_status = ${status}
+      //   WHERE id = ${id}
+      // `;
+      // } catch (error) {
+      //   if (error instanceof postgres.PostgresError) {
+      //     if (error.code == "22P02") {
+      //       return {
+      //         status: "error",
+      //         message: "type wrong.",
+      //       };
+      //     }
+
+      //     if (
+      //       error.code == "23505" &&
+      //       error.constraint_name == "room_number_key"
+      //     ) {
+      //       return {
+      //         status: "error",
+      //         message: "This room number is already taken.",
+      //       };
+      //     }
+      //   }
+
+      //   console.log(error);
+
+      //   return {
+      //     status: "error",
+      //     message: error,
+      //   };
+      // }
+
+      // return {
+      //   status: "success",
+      //   message: "Update successfully.",
+      // };
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        number: t.Optional(t.String()),
+        type_id: t.Optional(t.String()),
+        status: t.Optional(t.String()),
+      }),
+    }
+  )
   .delete("/delete/:id", async ({ params }) => {
     const { id } = params;
     try {
       await sql`DELETE FROM rooms
-                WHERE id=${id}`
+                WHERE id=${id}`;
     } catch (error) {
-      if ( error instanceof postgres.PostgresError ) {
-        if ( error.code == "22P02" ) {
+      if (error instanceof postgres.PostgresError) {
+        if (error.code == "22P02") {
           return {
-            status : 'error',
-            message : 'id is not uuid.'
-          }
-        }  
+            status: "error",
+            message: "id is not uuid.",
+          };
+        }
       }
-
-      console.log( error );
     }
 
     return {
-      status : 'success',
-      message : 'Your room have been removed.'
-    }
+      status: "success",
+      message: "Your room have been removed.",
+    };
   });

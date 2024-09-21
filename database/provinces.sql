cd6e3acb03b2a5a04741802c9de0aec1a72dd6f4
Mok <mokmaard646@gmail.com>
2024-09-22 00:14:01 +0700

    feat(provinces data): sql code for insert provinces.

---
+++ database/provinces.sql
@@ -0,0 +1,231 @@
+INSERT INTO provinces (code, name_en) 
+  VALUES (13, 'Pathum Thani');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (11, 'Samut Prakan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (12, 'Nonthaburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (10, 'Bangkok');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (14, 'Phra Nakhon Si Ayutthaya');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (16, 'Loburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (17, 'Sing Buri');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (15, 'Ang Thong');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (19, 'Saraburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (23, 'Trat');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (18, 'Chai Nat');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (25, 'Prachin Buri');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (20, 'Chon Buri');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (24, 'Chachoengsao');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (22, 'Chanthaburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (26, 'Nakhon Nayok');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (27, 'Sa Kaeo');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (31, 'Buri Ram');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (21, 'Rayong');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (33, 'Si Sa Ket');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (34, 'Ubon Ratchathani');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (32, 'Surin');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (35, 'Yasothon');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (37, 'Amnat Charoen');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (39, 'Nong Bua Lam Phu');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (41, 'Udon Thani');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (30, 'Nakhon Ratchasima');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (38, 'Bueng Kan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (42, 'Loei');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (40, 'Khon Kaen');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (43, 'Nong Khai');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (46, 'Kalasin');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (36, 'Chaiyaphum');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (45, 'Roi Et');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (44, 'Maha Sarakham');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (49, 'Mukdahan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (47, 'Sakon Nakhon');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (52, 'Lampang');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (48, 'Nakhon Phanom');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (51, 'Lamphun');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (55, 'Nan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (54, 'Phrae');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (58, 'Mae Hong Son');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (56, 'Phayao');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (50, 'Chiang Mai');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (61, 'Uthai Thani');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (60, 'Nakhon Sawan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (63, 'Tak');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (64, 'Sukhothai');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (62, 'Kamphaeng Phet');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (65, 'Phitsanulok');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (67, 'Phetchabun');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (66, 'Phichit');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (73, 'Nakhon Pathom');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (72, 'Suphan Buri');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (70, 'Ratchaburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (71, 'Kanchanaburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (74, 'Samut Sakhon');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (75, 'Samut Songkhram');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (81, 'Krabi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (57, 'Chiang Rai');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (80, 'Nakhon Si Thammarat');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (53, 'Uttaradit');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (82, 'Phangnga');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (83, 'Phuket');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (76, 'Phetchaburi');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (77, 'Prachuap Khiri Khan');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (84, 'Surat Thani');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (91, 'Satun');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (92, 'Trang');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (86, 'Chumphon');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (93, 'Phatthalung');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (95, 'Yala');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (90, 'Songkhla');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (96, 'Narathiwat');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (85, 'Ranong');
+
+INSERT INTO provinces (code, name_en) 
+  VALUES (94, 'Pattani');
+

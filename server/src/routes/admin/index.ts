7e67f4efedf9b83b624a6e5714e7e831163ed7b7
Mok <mokmaard646@gmail.com>
2024-09-22 19:30:25 +0700

    feat(index.ts): add use route.

--- server/src/routes/admin/index.ts
+++ server/src/routes/admin/index.ts
@@ -1,8 +1,10 @@
 import Elysia from 'elysia';
 import { roomTypeRoutes } from './roomType';
 import { employeeRoutes , resetPasswordRoutes } from './employee';
+import { customerRoutes } from './customer';
 
 export const adminRoutes = new Elysia({ prefix: '/admin' })
     .use(roomTypeRoutes)
     .use(employeeRoutes)
     .use(resetPasswordRoutes)
+    .use(customerRoutes)

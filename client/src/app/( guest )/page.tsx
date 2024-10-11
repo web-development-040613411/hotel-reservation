7b6c36138f4ebd3ff19e032649bf32e9bc5d96ad
Mok <mokmaard646@gmail.com>
2024-10-11 18:23:23 +0700

    feat(page): select date and amount of people.

--- client/src/app/( guest )/page.tsx
+++ client/src/app/( guest )/page.tsx
@@ -1,5 +1,25 @@
-export default function LandingPage() {
+import Image from "next/image"
+import FormBooking from "./components/form-booking"
+
+export default function Page() {
+  
+
   return (
-    <div>LandingPage</div>
+    <>
+      <div id="page1" className="w-100dvh h-100dvh">
+        <Image
+          src="/room.jpg"
+          width={500}
+          height={500}
+          alt="image"
+          />
+        <div className="md:w-96 w-3/4
+                        absolute 
+                        top-1/2 -translate-y-1/2
+                        left-1/2 -translate-x-1/2">
+          <FormBooking />
+        </div>
+      </div>
+    </>
   )
 }

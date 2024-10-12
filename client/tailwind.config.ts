8fbfc59a2834a3a4874ac2bcb548ac4b3717b77b
Mok <mokmaard646@gmail.com>
2024-10-12 10:45:53 +0700

    feat(tailwind): add primary color when hover.

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -22,7 +22,8 @@ const config: Config = {
   			},
   			primary: {
   				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				foreground: 'hsl(var(--primary-foreground))',
+					hover: 'hsl(var(--primary-hover))',
   			},
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',

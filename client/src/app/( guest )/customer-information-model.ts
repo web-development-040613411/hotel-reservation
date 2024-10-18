a4683c31f49883a4a0c7deb525ee8b6200b8f134
Mok <mokmaard646@gmail.com>
2024-10-18 21:43:47 +0700

    feat(variable): declare formInputs variable that hold array of form input information.

---
+++ client/src/app/( guest )/customer-information-model.ts
@@ -0,0 +1,58 @@
+export const formInputs = [
+  {
+    label: "First Name",
+    type: "text",
+    id: "first-name",
+    name: "first-name",
+    required: true,
+    key: "firstName"
+  },
+  {
+    label: "Last Name",
+    type: "text",
+    id: "last-name",
+    name: "last-name",
+    required: true,
+    key: "lastName"
+  },
+  {
+    label: "Address",
+    type: "text",
+    id: "address",
+    name: "address",
+    required: true,
+    key: "address"
+  },
+  {
+    label: "District",
+    type: "text",
+    id: "district",
+    name: "district",
+    required: true,
+    key: "district"
+  },
+  {
+    label: "Sub-District",
+    type: "text",
+    id: "sub-district",
+    name: "sub-district",
+    required: true,
+    key: "subDistrict"
+  },
+  {
+    label: "Phone Number",
+    type: "text",
+    id: "phone-number",
+    name: "phone-number",
+    required: true,
+    key: "phoneNumber"
+  },
+  {
+    label: "Email",
+    type: "email",
+    id: "email",
+    name: "email",
+    required: true,
+    key: "email"
+  },
+];

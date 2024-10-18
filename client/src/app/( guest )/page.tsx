a4b8658b51840a14f5665cce62d5763cb1e130fd
Mok <mokmaard646@gmail.com>
2024-10-18 21:45:30 +0700

    feat(main-guest-page): comprise all steps component.

--- client/src/app/( guest )/page.tsx
+++ client/src/app/( guest )/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Image from "next/image"
-import FormBooking from "./components/form-booking"
-import Step2 from "./components/step-2";
-import { ReservationContext, ReservationProvider} from "@/components/context/ReservationContext";
+import Step1 from "./components/step-1-selcect-date"
+import Step2 from "./components/step-2-select-room";
+import Step3 from "./components/step-3-input-customer-detail";
+import Step4 from "./components/step-4-confimation";
+import { ReservationProvider} from "@/components/context/ReservationContext";
 import { cn } from "@/lib/utils";
 import {useState } from "react";
 
 export default function Page() {
-  const [state, setState] = useState(1);
+  const [state, setState] = useState(4);
 
   return (
     <>
@@ -24,12 +26,20 @@ export default function Page() {
                           absolute 
                           top-1/2 -translate-y-1/2
                           left-1/2 -translate-x-1/2">
-            <FormBooking setState={setState}/>
+            <Step1 setState={setState}/>
           </div>
         </div>
 
         <div id="page2" className={cn('w-100dvh h-100dvh hidden', state == 2 && "block")}>
-          <Step2/>
+          <Step2 setState={setState}/>
+        </div>
+
+        <div id="page3" className={cn('w-100dvh h-100dvh hidden', state == 3 && "block")}>
+          <Step3 setState={ setState }/>
+        </div>
+
+        <div id="page4" className={cn('w-100dvh h-100dvh hidden', state == 4 && "block")}>
+          <Step4 setState={ setState }/>
         </div>
       </ReservationProvider>
     </>

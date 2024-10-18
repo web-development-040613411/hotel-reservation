e0c467f62d8b020b7ca1f565be6d59bddb9c39ba
Mok <mokmaard646@gmail.com>
2024-10-18 21:42:11 +0700

    feat(step-3): create component

---
+++ client/src/app/( guest )/components/step-3-input-customer-detail.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { useContext } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { ReservationContext } from "@/components/context/ReservationContext";
+import { formInputs } from "@/app/( guest )/customer-information-model";
+
+type Props = {
+  setState: (state: number) => void;
+};
+
+export default function Step3(prop: Props) {
+  const { addInformation, information } = useContext(ReservationContext);
+
+  const onSubmitHandler = (e : any) => {
+    e.preventDefault();
+
+    const personalInformation = {
+      firstName: e.target[0].value,
+      lastName: e.target[1].value,
+      address: e.target[2].value,
+      district: e.target[3].value,
+      subDistrict: e.target[4].value,
+      phoneNumber: e.target[5].value,
+      email: e.target[6].value,
+      specialRequests: e.target[7].value,
+    };
+
+    addInformation(personalInformation);
+    
+    prop.setState(4);
+  }
+
+  return (
+    <>
+      <div className="fixed bottom-0 bg-white
+                      w-full h-28 border-t-2 border-gray-400 flex items-center p-4 justify-around">
+        <div className="w-1/2">
+          <p className="font-bold">{ information.type_name }:
+            <span className="text-2xl font-black text-primary"> { information.date_range }</span>
+          </p>
+          <p className="font-bold">total:
+            <span className="text-2xl font-black text-primary"> 500 Bath</span>
+          </p>
+        </div>
+        
+        <Button
+              className=" bg-primary rounded-lg  hover:bg-gray-800 active:bg-gray-800
+                          py-6 hover:bg-primary-hover 
+                          font-bold text-white"
+              type="submit"
+              onClick={() => prop.setState(2)}
+            >
+              Change room type
+        </Button>
+      </div>
+
+      <div className="flex justify-center mt-5 mb-5">
+        <Card className="w-11/12 shadow-md border-primary shadow-primary m-0 p-8">
+          <CardHeader className="p-0">
+            <CardDescription className="text-lg">Step 3</CardDescription>
+            <CardTitle className="text-primary text-3xl font-black">
+              Personal Information
+            </CardTitle>
+          </CardHeader>
+
+          <form action="" className="my-4 flex flex-col gap-4"
+                onSubmit={(e) => onSubmitHandler(e)}>
+            {formInputs.map((input, index) => (
+              <div
+                key={index}
+                className="grid w-full max-w-sm items-center gap-1.5"
+              >
+                <Label htmlFor={input.id}>
+                  {input.label} <span className="text-red-500"> *</span>
+                </Label>
+                <Input
+                  type={input.type}
+                  id={input.id}
+                  placeholder=""
+                  name={input.name}
+                  className="my-2"
+                  required={input.required}
+                />
+              </div>
+            ))}
+
+            <div>
+              <Label htmlFor="email">Special Requests</Label>
+              <Textarea placeholder="Type your message here." className="my-2" />
+            </div>
+
+            <Button
+              className=" bg-primary w-full rounded-lg  hover:bg-gray-800 active:bg-gray-800
+                                  p-4 hover:bg-primary-hover 
+                                  font-bold text-white"
+              type="submit"
+            >
+              Confirm
+            </Button>
+          </form>
+        </Card>
+      </div>
+
+      <div className="h-28">
+      
+      </div>
+    </>
+  );
+}

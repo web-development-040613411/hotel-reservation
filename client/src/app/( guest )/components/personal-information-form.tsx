aea80027e4b374a4f87ff198212b064956416149
Mok <mokmaard646@gmail.com>
2024-10-30 13:02:23 +0700

    feat(responsive): input information.

--- client/src/app/( guest )/components/personal-information-form.tsx
+++ client/src/app/( guest )/components/personal-information-form.tsx
@@ -11,6 +11,9 @@ export default function PersonalInformationForm() {
     useContext(ReservationContext);
   const { personalInformation } = information;
   const [isChange, setIsChange] = useState(false);
+  const btnClass = `bg-primary w-full rounded-lg  hover:bg-gray-800 active:bg-gray-800
+                    p-4 my-4 hover:bg-primary-hover 
+                    font-bold text-white`
 
   const onSubmitHandler = (e: any) => {
     e.preventDefault();
@@ -25,73 +28,95 @@ export default function PersonalInformationForm() {
       postcode: e.target[6].value,
       phoneNumber: e.target[7].value,
       email: e.target[8].value,
-      specialRequests: e.target[9].value
+      specialRequests: e.target[9].value,
     };
 
     addInformation({ personalInformation });
 
     setIsChange(false);
 
-    if ( state != 4 ) {
+    if (state != 4) {
       setState(4);
-    }  
+    }
   };
 
   return (
     <form
-      action=""
-      className="my-4 flex flex-col gap-4"
+      className="my-4 flex flex-col
+                 md:flex-row md:w-full md:gap-10"
       onSubmit={(e) => onSubmitHandler(e)}
     >
-      {formInputs.map((input, index) => (
-        <div key={index} className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor={input.id}>
-            {input.label} <span className="text-red-500"> *</span>
-          </Label>
-          <Input
-            type={input.type}
-            id={input.id}
-            placeholder=""
-            name={input.name}
-            className="my-2"
-            required={input.required}
-            onChange={() => setIsChange(true)}
-            defaultValue={personalInformation ? personalInformation[input.key] : ""}
-          />
-        </div>
-      ))}
-
-      <div>
-        <Label htmlFor="email">Special Requests</Label>
-        <Textarea
-          placeholder="Type your message here."
-          className="my-2"
-          onChange={() => setIsChange(true)}
-          defaultValue={personalInformation ? personalInformation.specialRequests : ""}
-        />
+      <div className="md:w-1/2 md:flex md:flex-col md:gap-2">
+        {formInputs.slice(0, 6).map((input, index) => (
+          <div key={index}>
+            <Label htmlFor={input.id}>
+              {input.label} <span className="text-red-500"> *</span>
+            </Label>
+            <Input
+              type={input.type}
+              id={input.id}
+              placeholder=""
+              name={input.name}
+              className="my-2"
+              required={input.required}
+              onChange={() => setIsChange(true)}
+              defaultValue={
+                personalInformation ? personalInformation[input.key] : ""
+              }
+            />
+          </div>
+        ))}
       </div>
 
-      {
-        state == 4 ?
-        <Button
-        className=" bg-primary w-full rounded-lg  hover:bg-gray-800 active:bg-gray-800
-        p-4 hover:bg-primary-hover 
-        font-bold text-white"
-        type="submit"
-        disabled={!isChange}
-        >
-        Confirm Edit
-      </Button>
-      :
-      <Button
-        className=" bg-primary w-full rounded-lg  hover:bg-gray-800 active:bg-gray-800
-        p-4 hover:bg-primary-hover 
-        font-bold text-white"
-        type="submit"
-        >
-        Confirm
-      </Button>
-      }
+      <div className="md:w-1/2 md:flex-grow md:flex md:flex-col md:gap-2">
+        {formInputs.slice(6).map((input, index) => (
+          <div key={index}>
+            <Label htmlFor={input.id}>
+              {input.label} <span className="text-red-500"> *</span>
+            </Label>
+            <Input
+              type={input.type}
+              id={input.id}
+              placeholder=""
+              name={input.name}
+              className="my-2"
+              required={input.required}
+              onChange={() => setIsChange(true)}
+              defaultValue={
+                personalInformation ? personalInformation[input.key] : ""
+              }
+            />
+          </div>
+        ))}
+        <div className="md:flex-grow md:flex md:flex-col md:my-2 md:gap-2">
+          <Label htmlFor="email">Special Requests</Label>
+          <Textarea
+            placeholder="Type your message here."
+            className="md:flex-grow"
+            onChange={() => setIsChange(true)}
+            defaultValue={
+              personalInformation ? personalInformation.specialRequests : ""
+            }
+          />
+        </div>
+
+        {state == 4 ? (
+          <Button
+            className={btnClass}
+            type="submit"
+            disabled={!isChange}
+          >
+            Confirm Edit
+          </Button>
+        ) : (
+          <Button
+            className={btnClass}
+            type="submit"
+          >
+            Confirm
+          </Button>
+        )}
+      </div>
     </form>
   );
 }

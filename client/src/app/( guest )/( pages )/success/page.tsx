20018a6ff2cac4279b76c07c93bc36759bae6b67
Mok <mokmaard646@gmail.com>
2024-11-01 11:52:12 +0700

    refactor(success page): replace svg with check icon from Lucide.

--- client/src/app/( guest )/( pages )/success/page.tsx
+++ client/src/app/( guest )/( pages )/success/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Check } from "lucide-react";
 
 export default function Page() {
   const urlParam = new URLSearchParams(window.location.search);
@@ -9,44 +10,35 @@ export default function Page() {
 
   return (
     <>
-      <div
-        className="flex flex-col
-                      h-dvh
-                      items-center justify-center
-                      gap-8"
-      >
-        <svg
-          width="285"
-          height="214"
-          viewBox="0 0 285 214"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M25.274 95.9339L0.75 120.382L94.6385 213.981L284.25 24.9547L259.726 0.506348L94.6385 165.084L25.274 95.9339Z"
-            fill="#29AD32"
-          />
-        </svg>
-
-        <div className="text-center">
-          <h1 className="text-xl font-bold">You are all set.</h1>
-          <p>The reservation has send to your email address name <span className="font-black text-primary">{email}</span>,</p>
-          <p>please check your email for more details.</p>
+      <div className="w-full flex justify-center">
+        <div className="w-11/12 max-w-fit">
+          <div
+            className="flex flex-col
+                        h-dvh
+                        items-center justify-center
+                        gap-8"
+          >
+            <Check className="w-96 h-96 text-green-400"/>
+            <div className="text-center">
+              <h1 className="text-xl font-bold">You are all set.</h1>
+              <p>The reservation has send to your email address name <span className="font-black text-primary">{email}</span>,</p>
+              <p>please check your email for more details.</p>
+            </div>
+          </div>
+          <Link href="/">
+            <Button
+              className=" bg-primary w-11/12 max-w-96 rounded-lg  hover:bg-gray-800 active:bg-gray-800
+                          hover:bg-primary
+                          my-4
+                          font-bold text-white
+                          absolute bottom-20 left-1/2 -translate-x-1/2"
+              type="button"
+            >
+              Back to booking
+            </Button>
+          </Link>
         </div>
       </div>
-
-      <Link href="/">
-        <Button
-          className=" bg-primary w-11/12 rounded-lg  hover:bg-gray-800 active:bg-gray-800
-                      hover:bg-primary
-                      my-4
-                      font-bold text-white
-                      absolute bottom-20 left-1/2 -translate-x-1/2"
-          type="button"
-        >
-          Back to booking
-        </Button>
-      </Link>
     </>
   );
 }

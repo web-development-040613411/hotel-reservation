074abf64ce1b61cca2e5474d8d9fd7e878e310f0
Mok <mokmaard646@gmail.com>
2024-11-01 10:02:16 +0700

    feat(step-4-confirm-component): rm unused function and move conclusion bar to main component.

--- client/src/app/( guest )/components/step-4-confimation.tsx
+++ client/src/app/( guest )/components/step-4-confimation.tsx
@@ -2,96 +2,99 @@
 
 import { useContext, useState } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Card
-} from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { ReservationContext } from "@/context/ReservationContext";
 import RoomCard from "./room-card";
-import { loadStripe } from '@stripe/stripe-js';
-import ConclusionBar from "./conclusion-bar";
+import { loadStripe } from "@stripe/stripe-js";
 import PersonalInformationForm from "./personal-information-form";
 import StepHeader from "./header";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@radix-ui/react-collapsible";
+import { ArrowDown } from "lucide-react";
+import { ArrowUp } from "lucide-react";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 
 export default function Step4() {
-  const { addInformation, information, state, setState } = useContext(ReservationContext);
-  const [isChange, setIsChange] = useState(false);
+  const { information} = useContext(ReservationContext);
+  const [open, setIsOpen] = useState(false);
   const title = "Review";
   const step = 4;
 
   const { roomType, personalInformation, reservationId } = information;
-  const { type_id : roomTypeId } = roomType;
+  const { type_id: roomTypeId } = roomType;
 
-  const onSubmitHandler = (e: any) => {
-    e.preventDefault();
-
-    const personalInformation = {
-      firstName: e.target[0].value,
-      lastName: e.target[1].value,
-      address: e.target[2].value,
-      district: e.target[3].value,
-      subDistrict: e.target[4].value,
-      phoneNumber: e.target[5].value,
-      email: e.target[6].value,
-      specialRequests: e.target[7].value,
-    };
-
-    addInformation(personalInformation);
-  };
-
-  const  checkOut = async () => {
+  const checkOut = async () => {
     const stripe = await stripePromise;
     const totalPrice = roomType.total_price;
-    
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/stripe/checkout`, {
-      method: 'POST',
-      headers : {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        roomTypeId,
-        totalPrice,
-        personalInformation,
-        reservationId,
-      })
-    });
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/stripe/checkout`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          roomTypeId,
+          totalPrice,
+          personalInformation,
+          reservationId,
+        }),
+      }
+    );
 
     const data = await response.json();
     const sessionId = data.data.session_id;
 
     stripe!.redirectToCheckout({ sessionId });
-  }
-
-  const clickHandler = () => {
-    setState(2);
   };
 
   return (
     <>
-      <ConclusionBar/>
-
-      <div className="flex justify-center my-4">
-        <Card className="w-11/12 shadow-md border-primary shadow-primary m-0 p-8">
-          <StepHeader title={title} step={step}/>
-
-          <PersonalInformationForm/>
+      <div className="flex flex-col gap-8">
+  
+        <Card
+          className="shadow-md border-primary shadow-primary
+                     mt-5 p-8 mx-auto
+                     w-full"
+        >
+          <StepHeader title={title} step={step} />
+  
+          <Collapsible open={open} onOpenChange={setIsOpen} className="">
+            <CollapsibleTrigger asChild>
+              <Button className="hover:bg-primary-hover my-4">
+                {open ? (
+                  <ArrowDown className="h-4" />
+                ) : (
+                  <ArrowUp className="h-4" />
+                )}
+                User information
+              </Button>
+            </CollapsibleTrigger>
+  
+            <CollapsibleContent>
+              <PersonalInformationForm />
+            </CollapsibleContent>
+          </Collapsible>
         </Card>
-      </div>
-
-      <div className="p-8">
-        <RoomCard type={roomType} clickHandler={clickHandler} state={state} />
+  
+        <RoomCard type={roomType} />
+  
         <Button
-          className=" bg-primary w-full rounded-lg  hover:bg-gray-800 active:bg-gray-800
-                                    hover:bg-primary
-                                    my-4
-                                    font-bold text-white"
+          className=" bg-orange-400 w-full rounded-lg
+                      hover:bg-orange-200 active:bg-gray-800
+                        py-6
+                        font-bold text-white"
           type="button"
-          onClick={() => checkOut() }
+          onClick={() => checkOut()}
         >
           Make Payment
         </Button>
-      </div>
+</div>
 
       <div className="h-28"></div>
     </>

cd0eccf05e6434d22ebc89033c8c1ee4bb35b847
FuseSN2003 <sukavit.fuse2003@gmail.com>
2024-11-03 17:22:48 +0700

    Add check role

--- server/src/routes/admin/dashboard.ts
+++ server/src/routes/admin/dashboard.ts
@@ -1,26 +1,44 @@
-import { sql } from "@/libs/db";
-import { middleware } from "@/middleware";
-import Elysia from "elysia";
+import { sql } from '@/libs/db';
+import { middleware } from '@/middleware';
+import Elysia from 'elysia';
 
-export const dashboardRoutes = new Elysia({ prefix: "/dashboard" })
-  .use(middleware)
-  .get("/", async ({ user }) => {
-    const [totalBooking] = await sql`SELECT COUNT(*) FROM reservations`;
-    const [totalRevenue] = await sql`SELECT SUM(price) FROM reservations`;
-    const [totalGuest] = await sql`SELECT COUNT(customer_id) FROM reservations`;
-    const [availableRoom] = await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'vacant'`;
-    const [soldOutRoom] = await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'occupied'`;
-    const [offMarketRoom] = await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'off_market'`;
+export const dashboardRoutes = new Elysia({ prefix: '/dashboard' })
+    .use(middleware)
+    .get('/', async ({ user }) => {
+        // if (!user) {
+        //     return {
+        //         status: 'error',
+        //         message: 'Unauthorized',
+        //     };
+        // }
 
-    return {
-      status: "success",
-      data: {
-        totalBooking: totalBooking.count,
-        totalRevenue: totalRevenue.sum,
-        totalGuest: totalGuest.count,
-        availableRoom: availableRoom.count,
-        soldOutRoom: soldOutRoom.count,
-        offMarketRoom: offMarketRoom.count,
-      },
-    };
-  })
+        // if (user.role !== 'administrator') {
+        //     return {
+        //         status: 'error',
+        //         message: 'Forbidden',
+        //     };
+        // }
+
+        const [totalBooking] = await sql`SELECT COUNT(*) FROM reservations`;
+        const [totalRevenue] = await sql`SELECT SUM(price) FROM reservations`;
+        const [totalGuest] =
+            await sql`SELECT COUNT(customer_id) FROM reservations`;
+        const [availableRoom] =
+            await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'vacant'`;
+        const [soldOutRoom] =
+            await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'occupied'`;
+        const [offMarketRoom] =
+            await sql`SELECT COUNT(*) FROM rooms WHERE current_status = 'off_market'`;
+
+        return {
+            status: 'success',
+            data: {
+                totalBooking: totalBooking.count,
+                totalRevenue: totalRevenue.sum,
+                totalGuest: totalGuest.count,
+                availableRoom: availableRoom.count,
+                soldOutRoom: soldOutRoom.count,
+                offMarketRoom: offMarketRoom.count,
+            },
+        };
+    });

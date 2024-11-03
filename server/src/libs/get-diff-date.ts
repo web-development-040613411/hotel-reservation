f7b303dedf0165c53c9771bcd53b996fc72ce8ad
Mok <mokmaard646@gmail.com>
2024-11-03 16:17:47 +0700

    bug(diff date): change from math.ceil to floor.

--- server/src/libs/get-diff-date.ts
+++ server/src/libs/get-diff-date.ts
@@ -1,6 +1,6 @@
 export function getDiffDate(from: Date, to: Date) {
   const diffTime = Math.abs( from.setHours(0,0,0).valueOf() - to.valueOf());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
   return diffDays;
 }

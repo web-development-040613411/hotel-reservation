c0127e7ccccdb31a8cdacd98e65b5861fcad741a
FuseSN2003 <sukavit.fuse2003@gmail.com>
2024-11-03 17:14:31 +0700

    Dashboard api

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,75 +1,108 @@
 import { BedSingle, Coins, NotepadText, UserRound } from "lucide-react";
+import { headers } from "next/headers";
 
-export default function AdminPage() {
+async function getDashboardData(): Promise<
+  | { status: "error"; message: string }
+  | {
+      status: "success";
+      data: {
+        totalBooking: number;
+        totalRevenue: number;
+        totalGuest: number;
+        availableRoom: number;
+        soldOutRoom: number;
+        offMarketRoom: number;
+      };
+    }
+> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/dashboard`,
+    {
+      headers: headers(),
+    }
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function AdminPage() {
+  const result = await getDashboardData();
+
   return (
     <>
-      <div className="flex flex-col gap-8">
-        <div className="w-full grid grid-cols-3 gap-10">
-          <div className="flex items-center bg-background p-8 border shadow gap-4">
-            <div className="bg-primary text-primary-foreground p-2 rounded-md">
-              <NotepadText size={32} />
-            </div>
-            <div>
-              <p className="font-bold text-xl">200</p>
-              <p>total booking</p>
-            </div>
-          </div>
-          <div className="flex items-center bg-background p-8 border shadow gap-4">
-            <div className="bg-primary text-primary-foreground p-2 rounded-md">
-              <Coins size={32} />
-            </div>
-            <div>
-              <p className="font-bold text-xl">17k B</p>
-              <p>total revenue</p>
-            </div>
-          </div>
-          <div className="flex items-center bg-background p-8 border shadow gap-4">
-            <div className="bg-primary text-primary-foreground p-2 rounded-md">
-              <UserRound size={32} />
-            </div>
-            <div>
-              <p className="font-bold text-xl">449</p>
-              <p>total guest</p>
-            </div>
-          </div>
-        </div>
-        <div className="w-full bg-background p-4 border shadow space-y-4">
-          <h2 className="text-2xl font-bold">Reservation Stats</h2>
-          <div>Graph</div>
-        </div>
-        <div className="w-full bg-background p-4 border shadow space-y-4">
-          <h2 className="text-2xl font-bold">Today</h2>
-          <div className="grid grid-cols-3 gap-10">
+      {result.status === "success" ? (
+        <div className="flex flex-col gap-8">
+          <div className="w-full grid grid-cols-3 gap-10">
             <div className="flex items-center bg-background p-8 border shadow gap-4">
               <div className="bg-primary text-primary-foreground p-2 rounded-md">
-                <BedSingle size={32} />
+                <NotepadText size={32} />
               </div>
               <div>
-                <p className="font-bold text-xl">120</p>
-                <p>available room</p>
+                <p className="font-bold text-xl">{result.data.totalBooking}</p>
+                <p>total booking</p>
               </div>
             </div>
             <div className="flex items-center bg-background p-8 border shadow gap-4">
               <div className="bg-primary text-primary-foreground p-2 rounded-md">
-                <BedSingle size={32} />
+                <Coins size={32} />
               </div>
               <div>
-                <p className="font-bold text-xl">75</p>
-                <p>sold out room</p>
+                <p className="font-bold text-xl">{result.data.totalRevenue} B</p>
+                <p>total revenue</p>
               </div>
             </div>
             <div className="flex items-center bg-background p-8 border shadow gap-4">
               <div className="bg-primary text-primary-foreground p-2 rounded-md">
-                <BedSingle size={32} />
+                <UserRound size={32} />
               </div>
               <div>
-                <p className="font-bold text-xl">5</p>
-                <p>off market</p>
+                <p className="font-bold text-xl">{result.data.totalGuest}</p>
+                <p>total guest</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full bg-background p-4 border shadow space-y-4">
+            <h2 className="text-2xl font-bold">Reservation Stats</h2>
+            <div>Graph</div>
+          </div>
+          <div className="w-full bg-background p-4 border shadow space-y-4">
+            <h2 className="text-2xl font-bold">Today</h2>
+            <div className="grid grid-cols-3 gap-10">
+              <div className="flex items-center bg-background p-8 border shadow gap-4">
+                <div className="bg-primary text-primary-foreground p-2 rounded-md">
+                  <BedSingle size={32} />
+                </div>
+                <div>
+                  <p className="font-bold text-xl">{result.data.availableRoom}</p>
+                  <p>available room</p>
+                </div>
+              </div>
+              <div className="flex items-center bg-background p-8 border shadow gap-4">
+                <div className="bg-primary text-primary-foreground p-2 rounded-md">
+                  <BedSingle size={32} />
+                </div>
+                <div>
+                  <p className="font-bold text-xl">{result.data.soldOutRoom}</p>
+                  <p>sold out room</p>
+                </div>
+              </div>
+              <div className="flex items-center bg-background p-8 border shadow gap-4">
+                <div className="bg-primary text-primary-foreground p-2 rounded-md">
+                  <BedSingle size={32} />
+                </div>
+                <div>
+                  <p className="font-bold text-xl">{result.data.offMarketRoom}</p>
+                  <p>off market</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <p>{result.message}</p>
+      )}
     </>
   );
 }
